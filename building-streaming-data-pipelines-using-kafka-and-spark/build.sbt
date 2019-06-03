664d7ece6665b31099c01173af1193832b416824
Chema <josem32832@gmail.com>
2019-06-03 15:19:06 +0200

    03062019

--- building-streaming-data-pipelines-using-kafka-and-spark/build.sbt
+++ building-streaming-data-pipelines-using-kafka-and-spark/build.sbt
@@ -10,13 +10,21 @@ libraryDependencies += "org.apache.kafka" % "kafka-clients" % "1.0.0"
 
 libraryDependencies += "com.maxmind.geoip2" % "geoip2" % "2.12.0"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.0"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.2.0"
 
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.3.0"
+libraryDependencies += "org.apache.spark" % "spark-sql_2.11" % "2.2.0"
 
+libraryDependencies += "org.apache.spark" % "spark-sql-kafka-0-10_2.11" % "2.2.0"
+
+dependencyOverrides += "org.apache.spark" % "spark-sql_2.11" % "2.2.0"
 dependencyOverrides += "com.fasterxml.jackson.core" % "jackson-core" % "2.9.5"
 dependencyOverrides += "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.5"
 dependencyOverrides += "com.fasterxml.jackson.module" % "jackson-module-scala_2.11" % "2.9.5"
 
 
 // /home/cloudera/.ivy2/cache/com.typesafe/config/bundles/config-1.3.2.jar
+// /home/cloudera/.ivy2/cache/org.apache.spark/spark-sql_2.11/jars/spark-sql_2.11-2.3.0.jar
+// /home/cloudera/.ivy2/cache/org.apache.kafka/kafka-clients/jars/kafka-clients-1.0.0.jar
+// /home/cloudera/.ivy2/cache/org.apache.spark/spark-sql-kafka-0-10_2.11/jars/spark-sql-kafka-0-10_2.11-2.3.0.jar
+// /home/cloudera/.ivy2/cache/org.apache.spark/spark-sql_2.11/jars/spark-sql_2.11-2.1.0.jar
+// /home/cloudera/.ivy2/cache/org.apache.spark/spark-core_2.11/jars/spark-core_2.11-2.3.0.jar
---
+++ building-streaming-data-pipelines-using-kafka-and-spark/src/main/scala/structuredstreamingintegration/GetStreamingDepartmentTrafficKafka.scala
@@ -0,0 +1,54 @@
+package structuredstreamingintegration
+
+import java.sql.Timestamp
+
+import com.typesafe.config.ConfigFactory
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.streaming.Trigger
+
+// Created by itversity on 19/05/18.
+
+object GetStreamingDepartmentTrafficKafka {
+
+  def main(args: Array[String]): Unit = {
+
+    val conf = ConfigFactory.load.getConfig(args(0))
+    val spark = SparkSession.
+      builder().
+      master(conf.getString("execution.mode")).
+      appName("Get Streaming Department Traffic").
+      getOrCreate()
+
+    import spark.implicits._
+    spark.sparkContext.setLogLevel("ERROR")
+    spark.conf.set("spark.sql.shuffle.partitions", "2")
+
+    val lines: Dataset[(String, Timestamp)] = spark.readStream.
+      format("kafka").
+      option("kafka.bootstrap.servers", conf.getString("bootstrap.servers")).
+      option("subscribe", "retail_multi").
+      option("includeTimestamp", true).
+      load.
+      selectExpr("CAST(value AS STRING)", "timestamp").
+      as[(String, Timestamp)]
+
+    val departmentTraffic: DataFrame = lines.
+      where(split(split($"value", " ")(6), "/")(1) === "department").
+      select(split(split($"value", " ")(6), "/")(2).alias("department_name"), $"timestamp").
+      groupBy(
+        window($"timestamp", "20 seconds", "20 seconds"),$"department_name"
+      ).
+      count().toDF("window","department_name","count")
+
+    val query = departmentTraffic.
+      writeStream.
+      outputMode("update").
+      format("console").
+      trigger(Trigger.ProcessingTime("20 seconds")).
+      start()
+
+    query.awaitTermination()
+  }
+
+}
